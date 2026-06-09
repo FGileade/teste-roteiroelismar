fdafe01abfc8ae02de07a9361d1fd82662192a5a
FGileade <filipegileade@gmail.com>
2026-06-09 08:04:56 -0300

    fix: converter require do postcss para import esm

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
-// Força o processamento explícito do CSS no build da Vercel
+// Força o processamento explícito do CSS no build da Vercel utilizando ESM
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
+        tailwindcss,
+        autoprefixer,
       ],
     },
   },
